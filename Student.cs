ccbc0d080df508363ffd019e4818900f5ace55e6
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-14 11:58:11 +0300

    Student and Teacher class made and tested

---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example4
+{
+    internal class Student
+    {
+        private string fname;
+        public void SetName(string value)
+        {
+            this.fname = value;
+        }
+        public string GetName()
+        {
+            return this.fname;
+        }
+    }
+}
